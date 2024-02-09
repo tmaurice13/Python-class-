fd621f89f13d7bf6b8b8cce496f2e8d0512f57ab
tmaurice13 <mauricetabaruka@gmail.com>
2024-02-09 05:01:20 -0800

    assignments

---
+++ 2Darray/while.py
@@ -0,0 +1,7 @@
+sum = 0
+count = 1
+while count <=10:
+    sum +=count
+    count += 1
+# print("sum to 10: ", sum)
+
---
+++ conditions/switch_casese.py
@@ -0,0 +1,7 @@
+def switch_case(argument):
+    if argument=='a':
+        return 'apple' 
+    elif  argument=='b':
+        return 'banana'
+    elif argument=='c':
+          return 'cherry' 
